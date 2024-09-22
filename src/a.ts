91cd1e1bb7b2fa8502432789392cfc9c52b3a61e
samar-abbas-786 <samarabbas172003@gmail.com>
2024-09-23 04:02:04 +0530

    Interface

--- src/a.ts
+++ src/a.ts
@@ -1,28 +1,60 @@
-// const x:number=1;
-// console.log(x);
+// // const x:number=1;
+// // console.log(x);
 
-function greet(firstName:string){
-    console.log("Hello" + firstName);
+// function greet(firstName:string){
+//     console.log("Hello" + firstName);
     
-}
-greet("Samar Abbas")
+// }
+// greet("Samar Abbas")
 
 
-function sum(a:number,b:number):number{
-    return a+b;
-}
-let ans=sum(1,2);
-console.log(ans);
+// function sum(a:number,b:number):number{
+//     return a+b;
+// }
+// let ans=sum(1,2);
+// console.log(ans);
 
 
-function runAfterT(fn:()=>number){
-    setTimeout(fn,1000);
-};
+// function runAfterT(fn:()=>number){
+//     setTimeout(fn,1000);
+// };
 
-runAfterT(()=>{
-    console.log("Hii There");
+// runAfterT(()=>{
+//     console.log("Hii There");
   
 
-    return 1;
+//     return 1;
     
-})
+// })
+
+
+
+// function isLegal(user:{
+//     name:string,age:number,email:string
+// }){
+//     if(user.age>19){
+//         console.log("Legal");
+        
+//     }
+//     else{
+//         console.log("Not Legal");
+        
+//     }
+// }
+
+interface User{
+    name:string,
+    age:number,
+    email:string
+}
+
+function isLegal(user:User){
+    if(user.age>18){
+        console.log("Legal");
+        
+    }
+    else{
+        console.log("Not Legal");
+        
+    }
+}
